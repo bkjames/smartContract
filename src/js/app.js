fa56b4edfd39aeb6529080b656c3b6ac9c4c608b
bkjames <sweden011@gmail.com>
2018-11-20 16:19:57 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -17,18 +17,52 @@ App = {
         list.append(template.html());
       }
     })
+
+    return App.initWeb3();
   },
 
   initWeb3: function() {
-	
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
   },
 
   initContract: function() {
-		
+	$.getJSON('RealEstate.json', function(data) {
+      App.contracts.RealEstate = TruffleContract(data);
+      App.contracts.RealEstate.setProvider(App.web3Provider);
+    });
   },
 
   buyRealEstate: function() {	
+    var id = $('#id').val();
+    var name = $('#name').val();
+    var price = $('#price').val();
+    var age = $('#age').val();
 
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+      App.contracts.RealEstate.deployed().then(function(instance) {
+        var nameUtf8Encoded = utf8.encode(name);
+        return instance.buyRealEstate(id, web3.toHex(nameUtf8Encoded), age, { from: account, value: price });
+      }).then(function() {
+        $('#name').val('');
+        $('#age').val('');
+        $('#buyModal').modal('hide');       
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   },
 
   loadRealEstates: function() {
@@ -43,5 +77,13 @@ App = {
 $(function() {
   $(window).load(function() {
     App.init();
+  });
+
+  $('#buyModal').on('show.bs.modal', function(e) {
+    var id = $(e.relatedTarget).parent().find('.id').text();
+    var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
+
+    $(e.currentTarget).find('#id').val(id);
+    $(e.currentTarget).find('#price').val(price);
   });
 });
