7c2c4555eb616029be2687b5b20a71603791bbc6
bkjames <sweden011@gmail.com>
2018-11-20 15:34:16 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,20 @@ App = {
   contracts: {},
 	
   init: function() {
-   
+    $.getJSON('../real-estate.json', function(data) {
+      var list = $('#list');
+      var template = $('#template');
+
+      for (i = 0; i < data.length; i++) {
+        template.find('img').attr('src', data[i].picture);
+        template.find('.id').text(data[i].id);
+        template.find('.type').text(data[i].type);
+        template.find('.area').text(data[i].area);
+        template.find('.price').text(data[i].price);
+
+        list.append(template.html());
+      }
+    })
   },
 
   initWeb3: function() {
